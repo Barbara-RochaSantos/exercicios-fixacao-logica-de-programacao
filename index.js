7e40a3329ca54d8da87d1c308cd4b8673fd724cc
Barbara-RochaSantos <contato.barbararochasantos@gmail.com>
2025-07-02 00:29:12 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Exercícios de Lógica de Programação
+// Exercícios de Laços - FOR 
+// Nivel Fácil
+
+// Imprimir números de 1 a 10
+
+for (let i = 1 ; i <= 10 ; i ++){
+	console.log (i)
+    }
+
